aa602ec02070ade58de0247c20df4f9f5256899e
dengoyoon <engus927@naver.com>
2022-09-08 17:38:55 +0900

    메인 액티비티 마운트 시킴

--- pages/components/MainActivity.js
+++ pages/components/MainActivity.js
@@ -1,8 +1,12 @@
 import Activity from "../core/Activity.js";
+import Component from "../core/Component.js";
+import State from "../core/State.js";
 
 export default class MainActivity extends Activity {
     constructor(selector, props) {
         super(selector, props);
+        this._state = new State({});
+        this.render();
     }
 
     template() {

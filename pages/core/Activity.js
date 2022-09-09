991479451b70059fcc688476e119ee27f094f03e
dengoyoon <engus927@naver.com>
2022-09-09 23:48:17 +0900

    activity 개선 중

--- pages/core/Activity.js
+++ pages/core/Activity.js
@@ -7,14 +7,8 @@ export default class Activity extends Component {
     // activityStack에 push / pop 조작
     // 직접적으로 필요한가 싶기도 한데 일단은 제작
     _activityId;
-    _isDisplay;
     constructor(selector, props) {
         super(selector, props);
         this._activityId = getActivityId(selector);
-        this._isDisplay = true;
-    }
-
-    getFrameHtml() {
-        return `<div id=${this._activityId}></div>`;
     }
 }

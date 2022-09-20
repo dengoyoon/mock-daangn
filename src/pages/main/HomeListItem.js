b2f6836d0997019fe8f568a2d66cadd302b206e3
dengoyoon <engus927@naver.com>
2022-09-20 16:57:24 +0900

    홈화면 리스트 레이아웃 완성

--- src/pages/main/HomeListItem.js
+++ src/pages/main/HomeListItem.js
@@ -2,6 +2,9 @@ import Component from "../../core/Component.js";
 
 import "../../stylesheets/item-home-list.scss";
 
+import ic_chatroom from "../../img/ic_chatroom.png";
+import ic_heart from "../../img/ic_heart.png";
+
 export default class HomeListItem extends Component {
     _item;
 
@@ -17,14 +20,37 @@ export default class HomeListItem extends Component {
         this.mounted();
     }
 
+    showHeart() {
+        return this._item.likeNum === undefined ? "hide" : "";
+    }
+
+    showChatroom() {
+        return this._item.chatRoomNum === undefined ? "hide" : "";
+    }
+
     template() {
         return `
             <div class='home--item'>
-                <img class='home--item--img' src='${this._item.productImageUrl}'/>
-                <div class='home--item--content'>
-                    <div class='home--item--content--name'>${this._item.productName}</div>
-                    <div class='home--item--content--location'>${this._item.locationName} · ${this._item.ago}</div>
-                    <div class='home--item--content--price'>${this._item.price}원</div>
+                <img class='item--img' src='${this._item.productImageUrl}'/>
+                <div class='item--content'>
+                    <div class='item--content--name'>${this._item.productName}</div>
+                    <div class='item--content--location'>
+                        ${this._item.locationName} · ${this._item.ago}</div>
+                    <div class='item--content--price'>${this._item.price}원</div>
+                    <div class='item--content--likeandchat'>
+                        <div class='likeandchat--element ${this.showChatroom()}'>
+                            <img class='likeandchat--element--icon' src='${ic_chatroom}'/>
+                            <span class='likeandchat--element--text'>
+                                ${this._item.chatRoomNum}
+                            </span>
+                        </div>
+                        <div class='likeandchat--element ${this.showHeart()}'>
+                            <img class='likeandchat--element--icon' src='${ic_heart}'/> 
+                            <span class='likeandchat--element--text'>
+                                ${this._item.likeNum}
+                            </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         `;

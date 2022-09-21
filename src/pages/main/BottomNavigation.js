bc2667d2a4e7c9d4854d277f00e60c7a2b946577
dengoyoon <engus927@naver.com>
2022-09-21 15:46:57 +0900

    bottom navigation 퍼블리싱 완료

--- src/pages/main/BottomNavigation.js
+++ src/pages/main/BottomNavigation.js
@@ -1,5 +1,10 @@
 import Component from "../../core/Component.js";
 
+import "../../stylesheets/bottom-navigation.scss";
+
+import ic_home_filled from "../../img/ic_home_filled.png";
+import ic_chat_empty from "../../img/ic_chat_empty.png";
+
 export default class BottomNavigation extends Component {
     constructor(selector, props) {
         super(selector, props);
@@ -8,7 +13,14 @@ export default class BottomNavigation extends Component {
 
     template() {
         return `
-            <div>HI Hi</div>
+            <button type="button" class="bottom--button">
+                <img class = "bottom--button--img" src='${ic_home_filled}'/>
+                <div class = "bottom--button--text">홈</div>
+            </button>
+            <button type="button" class="bottom--button">
+                <img class = "bottom--button--img" src='${ic_chat_empty}'/>
+                <div class = "bottom--button--text">채팅</div>
+            </button>
         `;
     }
 }

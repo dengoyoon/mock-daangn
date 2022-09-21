29059943029b0edb3f30d4705c89695c6cd1e49c
dengoyoon <engus927@naver.com>
2022-09-21 15:15:29 +0900

    Bottom Navigation 마운트 성공

---
+++ src/pages/main/BottomNavigation.js
@@ -0,0 +1,14 @@
+import Component from "../../core/Component.js";
+
+export default class BottomNavigation extends Component {
+    constructor(selector, props) {
+        super(selector, props);
+        this.render();
+    }
+
+    template() {
+        return `
+            <div>HI Hi</div>
+        `;
+    }
+}

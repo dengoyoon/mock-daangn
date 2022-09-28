8825157f9450f6cd1ac0589ca2943e224c23fab9
dengoyoon <engus927@naver.com>
2022-09-29 01:27:04 +0900

    캐러셀... 어려워요

--- src/pages/detail/ImageCarousel.js
+++ src/pages/detail/ImageCarousel.js
@@ -8,9 +8,19 @@ import dummy_bear3 from "../../img/dummy_bear3.jpeg";
 import dummy_bear4 from "../../img/dummy_bear4.jpeg";
 
 export default class ImageCarousel extends Component {
+    _currentImageIndex;
+    _carouselSlide;
+    _carouselContents;
+    _moveSize;
+
     constructor(selector, props) {
         super(selector, props);
+        this._currentImageIndex = 0;
         this.render();
+
+        this._carouselSlide = document.querySelector(".slide_list");
+        this._carouselContents = document.querySelectorAll(".slide_item");
+        this._moveSize = this._carouselContents[0].clientWidth;
         this.setEvent();
     }
 
@@ -18,13 +28,18 @@ export default class ImageCarousel extends Component {
         return `
             <div class="slide_box">
                 <div class="slide_list">
-                    <div id="lastClone" class="slide_item">5</div>
-                    <div class="slide_item">1</div>
-                    <div class="slide_item">2</div>
-                    <div class="slide_item">3</div>
-                    <div class="slide_item">4</div>
-                    <div class="slide_item">5</div>
-                    <div id="firstClone" class="slide_item">1</div>
+                    <div class="slide_item">
+                        <img  class='slide_item--img' src='${dummy_bear1}'/>
+                    </div>
+                    <div class="slide_item">
+                        <img  class='slide_item--img' src='${dummy_bear2}'/>
+                    </div>
+                    <div class="slide_item">
+                        <img  class='slide_item--img' src='${dummy_bear3}'/>
+                    </div>
+                    <div class="slide_item">
+                        <img  class='slide_item--img' src='${dummy_bear4}'/>
+                    </div>
                 </div>
             </div>
             <button class="prevBtn">prev</button>
@@ -32,33 +47,23 @@ export default class ImageCarousel extends Component {
         `;
     }
 
+    onClickNext(event) {
+        if (this._currentImageIndex >= this._carouselContents.length - 1) return;
+        this._currentImageIndex++;
+        this._carouselSlide.style.transform =
+            "translateX(" + -this._moveSize * this._currentImageIndex + "px)";
+    }
+
+    onClickPrev(event) {
+        if (this._currentImageIndex <= 0) return;
+        this._currentImageIndex--;
+        this._carouselSlide.style.transform =
+            "translateX(" + -this._moveSize * this._currentImageIndex + "px)";
+    }
+
     setEvent() {
         // 일단 ,,, 해봤다
-        const carouselSlide = document.querySelector(".slide_list");
-        const carouselContents = document.querySelectorAll(".slide_item");
-
-        const prevBtn = document.querySelector(".prevBtn");
-        const nextBtn = document.querySelector(".nextBtn");
-
-        //counter
-        let counter = 1;
-        const size = carouselContents[0].clientWidth;
-
-        carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-
-        nextBtn.addEventListener("click", () => {
-            if (counter >= carouselContents.length - 1) return;
-            carouselSlide.style.transition = "transform 0.3s ease-in-out";
-            counter++;
-            carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-        });
-
-        prevBtn.addEventListener("click", () => {
-            if (counter <= 0) return;
-            carouselSlide.style.transition = "transform 0.3s ease-in-out";
-            counter--;
-            carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-        });
-        console.log(carouselContents.length);
+        this.addEvent("click", ".nextBtn", this.onClickNext.bind(this));
+        this.addEvent("click", ".prevBtn", this.onClickPrev.bind(this));
     }
 }
